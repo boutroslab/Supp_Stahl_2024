7b0ad627d01c14ce569bc417fc195258c13291dc
Florian Heigwer <florian_heigwer@web.de>
2024-02-19 21:04:26 +0100

    update

--- scripts/mageck_command.sh
+++ scripts/mageck_command.sh
@@ -1,4 +1,4 @@
 mageck count -l ../annotations/mageck_library_A.csv -n ../data/mapped/SARS-CRISPR-Deep --sample-label \
-AS-497825-LR-51404_R1,AS-497827-LR-51404_R1,AS-497829-LR-51404_R1,AS-497831-LR-51404_R1 \
+AS-497825-LR-51404_R1,AS-497827-LR-51404_R1,AS-497829-LR-51404_R1,AS-497831-LR-51404_R1,AS-497833-LR-51404_R1,AS-497835-LR-51404_R1,AS-497837-LR-51404_R1,AS-497839-LR-51404_R1 \
 --fastq \
-../data/fastq/AS-497825-LR-51404_R1.fastq.gz ../data/fastq/AS-497827-LR-51404_R1.fastq.gz ../data/fastq/AS-497829-LR-51404_R1.fastq.gz ../data/fastq/AS-497831-LR-51404_R1.fastq.gz
+../data/fastq/AS-497825-LR-51404_R1.fastq.gz ../data/fastq/AS-497827-LR-51404_R1.fastq.gz ../data/fastq/AS-497829-LR-51404_R1.fastq.gz ../data/fastq/AS-497831-LR-51404_R1.fastq.gz ../data/fastq/AS-497833-LR-51404_R1.fastq.gz ../data/fastq/AS-497835-LR-51404_R1.fastq.gz ../data/fastq/AS-497837-LR-51404_R1.fastq.gz ../data/fastq/AS-497839-LR-51404_R1.fastq.gz
